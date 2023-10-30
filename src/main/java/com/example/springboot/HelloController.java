55c9c24369d0e70c32f592b837c895e6b96303e9
Corby Page <pcorby@vmware.com>
2023-10-30 13:51:39 -0500

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from TAP + ADO!";
+		return "Greetings from Tanzu Application Platform + ADO!";
 	}
 
 }
